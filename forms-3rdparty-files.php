6b6ef0485901581326e7f707b29f5b1dfb3889e4
zaus <dr.zaus+dev@gmail.com>
2015-10-23 09:31:14 -0400

    wip: attach/how

--- forms-3rdparty-files.php
+++ forms-3rdparty-files.php
@@ -15,6 +15,27 @@ Changelog:
 abstract class F3i_Files_Base {
 	const B = 'Forms3rdPartyIntegration';
 
+	/**
+	 * Setting for how to include the attachments:  path (unchanged), url, raw/binary, base64
+	 */
+	const OPTION_ATTACH_HOW = 'f3if_how';
+	/**
+	 * Setting for what index to include the attachments
+	 */
+	const OPTION_ATTACH_AS = 'f3if_entry';
+
+	const VAL_ATTACH_PATH = 'path';
+	const VAL_ATTACH_LINK = 'url';
+	const VAL_ATTACH_RAW = 'raw';
+	const VAL_ATTACH_BAS64 = 'base64';
+
+	/**
+	 * Where the files should show up in the submission array for mapping
+	 */
+	const SUBMISSION_ATTACH = '_FILES_';
+
+	static $_once = false;
+
 	function __construct() {
 		// expose files through submission $post array -- makes it available to mappings
 		add_filter(self::B.'_get_submission', array(&$this, 'get_submission'), 11, 2);
@@ -22,7 +43,13 @@ abstract class F3i_Files_Base {
 		// if you don't want user to need to actually type in the mapping
 		add_filter(self::B.'_service_filter_post', array(&$this, 'automap'), 11, 5);
 
-		$this->_file_entry = 'FILES'; // or get from a configurable wp_option?
+		// configure whether to attach or not, how; because this is a base class only do it once
+		if(!self::$_once) {
+			add_filter(self::B.'_service_settings', array(&$this, 'service_settings'), 10, 3);
+			self::$_once = true;
+		}
+
+		$this->_file_entry = self::SUBMISSION_ATTACH; // or get from a configurable wp_option?
 	}
 
 	private $_file_entry; // alias to where we stick it in the submission/post array
@@ -34,7 +61,7 @@ abstract class F3i_Files_Base {
 	abstract protected function get_files();
 
 	public function automap($post, $service, $form, $sid, $submission) {
-		// apply shortcodes (or maybe just settings) to either:
+		// todo: apply shortcodes (or maybe just settings) to either:
 		// 1. link to the file
 		// 1-5. name of the file
 		// 2. include the bytes
@@ -42,8 +69,14 @@ abstract class F3i_Files_Base {
 		// 4. gzip the bytes
 		// 6. shortcodes would allow combinations of the above
 		
-		// filter result so others can add more stuff?
-		$post[$this->_file_entry] = $submission[$this->_file_entry];
+		// todo: filter result so others can add more stuff? probably unnecessary, can just attach later filter
+
+		// not configured? ignore
+		if(empty($service[self::OPTION_ATTACH_AS])) return $post;
+
+
+		$post[$service[self::OPTION_ATTACH_AS]] = $submission[$this->_file_entry];
+
 		return $post;
 	}
 	
@@ -58,6 +91,31 @@ abstract class F3i_Files_Base {
 		if(is_plugin_active('contact-form-7/wp-contact-form-7.php') || class_exists('WPCF7_ContactForm') ) new F3i_CF7_Files;
 		//if(is_plugin_active('ninja-forms/ninja-forms.php') || class_exists('Ninja_Forms') ) new F3i_Ninja_Files;
 	}
+
+	public function service_settings($eid, $P, $entity) {
+	?>
+		<fieldset><legend><span><?php _e('File Attachments', $P); ?></span></legend>
+			<div class="inside">
+				<em class="description">How to attach files to submission mappings.</em>
+
+				<?php $field = self::OPTION_ATTACH_AS; ?>
+				<div class="field">
+					<label for="<?php echo $field, '-', $eid ?>"><?php _e('Map attachments as:', $P); ?></label>
+					<input id="<?php echo $field, '-', $eid ?>" type="text" class="text" name="<?php echo $P, '[', $eid, '][', $field, ']'?>" value="<?php echo isset($entity[$field]) ? esc_attr($entity[$field]) : self::SUBMISSION_ATTACH ?>" />
+					<em class="description"><?php _e('To what field name the file(s) will be mapped to in the post submission.  Leave blank to omit (and allow you to manually map it instead; note this will not apply transformations).', $P); ?></em>
+				</div>
+				<?php $field = self::OPTION_ATTACH_HOW; ?>
+				<div class="field">
+					<label for="<?php echo $field, '-', $eid ?>"><?php _e('Attachment style:', $P); ?></label>
+					TODO: select box
+					<input id="<?php echo $field, '-', $eid ?>" type="checkbox" class="checkbox" name="<?php echo $P, '[', $eid, '][', $field, ']'?>" value="yes"<?php echo isset($entity[$field]) ? ' checked="checked"' : ''?> />
+					<em class="description"><?php _e('How to include file attachments.', $P); ?></em>
+				</div>
+			</div>
+		</fieldset>
+	<?php
+	}
+
 }
 F3i_Files_Base::init();
 
